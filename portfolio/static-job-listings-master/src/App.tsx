253eb9a8b2945f86a3dd505ac229ba87eab51d70
pedro-isacss <ss.pedroisac@gmail.com>
2020-10-17 22:22:16 -0300

    Add static-job-listings-master

--- portfolio/static-job-listings-master/src/App.tsx
+++ portfolio/static-job-listings-master/src/App.tsx
@@ -1,70 +1,131 @@
-import React, { useEffect, useState } from 'react';
-import './App.css'
-import businessLogo from './images/business-logo.png'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import businessLogo from "./images/business-logo.png";
+
+interface Data {
+  id: string;
+  company_logo: string;
+  company: string;
+  url: string;
+  title: string;
+  type: string;
+  location: string;
+  description: string;
+  created_at?: string;
+  company_url?: string;
+  how_to_apply?: string;
+}
 
 function App() {
-  const [data, setData] = useState([
-    {
-      id: "",
-      company_logo: "",
-      company: "",
-      url: "",
-      title: "",
-      type: "",
-      location: "",
-      description: "",
-    }
-  ])
-  const [tags, setTags] = useState<String[]>([])
-  let defaultTags = ["HTML", "CSS", "JavaScript", "Frontend", "Junior", "Senior", "Python",
-   "Ruby", "C#", "Kotlin", "React", "Flutter", "React Native", "Vue", "Angular"]
+  const [data, setData] = useState<Data[]>([]);
+  const [dataFiltered, setDataFiltered] = useState<Data[]>([]);
+  const [tags, setTags] = useState<string[]>([]);
+  let defaultTags = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Frontend",
+    "Junior",
+    "Senior",
+    "Python",
+    "Ruby",
+    "C#",
+    "Kotlin",
+    "React",
+    "Flutter",
+    "React Native",
+    "Vue",
+    "Angular",
+  ];
 
   useEffect(() => {
-    fetch("https://api.allorigins.win/raw?url=https://jobs.github.com/positions.json").
-    then(response => response.json()).
-    then((res) => {
-      setData(res)
-    })
-    if(tags !== []){
-      setData([])
-      let i: String
-      for(i of tags){
-        data.map(job => {
-          if(job["description"].toUpperCase().indexOf(i.toUpperCase()) !== -1){
-            setData([...data, job])
-          }
-        })
-      }
-    }
-  }, [tags])
+    getJobs();
+  }, []);
+
+  function filterJobs(t: string) {
+    setData(
+      data.filter(
+        (job) =>
+          job["description"].toUpperCase().indexOf(t.toUpperCase()) !== -1
+      )
+    );
+  }
+
+  async function getJobs() {
+    await fetch(
+      "https://api.allorigins.win/raw?url=https://jobs.github.com/positions.json"
+    )
+      .then((response) => response.json())
+      .then((res) => {
+        setData(res);
+      });
+  }
 
   return (
     <div className="App">
       <header className="header">
         <div className="search">
           <div className="search-tags">
-          {tags.map(tag => {
-            return(
-            <div>
-              <span>{tag}</span>
-              <button>x</button>
-            </div>
-            )
-          })}
+            {tags.map((tag) => {
+              return (
+                <div>
+                  <span>{tag}</span>
+                  <button
+                    onClick={() => {
+                      setTags(tags.filter((item) => item !== tag));
+                      if (tags.filter((item) => item !== tag).length === 0) {
+                        getJobs();
+                      } else {
+                        setData([
+                          {
+                            id: "string",
+                            company_logo: "string",
+                            company: "string",
+                            url: "string",
+                            title: "string",
+                            type: "string",
+                            location: "string",
+                            description: "string",
+                          },
+                        ]);
+                        getJobs().then(() => {
+                          tags.map((item) => filterJobs(item));
+                          console.log(data);
+                        });
+                      }
+                    }}
+                  >
+                    x
+                  </button>
+                </div>
+              );
+            })}
           </div>
           <div className="btn-clear-container">
-            <button>Clear</button>
+            <button
+              onClick={() => {
+                setTags([]);
+                getJobs();
+              }}
+            >
+              Clear
+            </button>
           </div>
         </div>
       </header>
       <div className="jobs-list-container">
-        {
-        data.map(job => {
-          return(
+        {data.map((job) => {
+          return (
             <div className="job-container" key={job["id"]}>
               <div className="job-info-container">
                 <div className="job-img-container">
-                  <img src={job["company_logo"] !== null?job["company_logo"]:businessLogo} />
+                  <img
+                    src={
+                      job["company_logo"] !== null
+                        ? job["company_logo"]
+                        : businessLogo
+                    }
+                  />
                 </div>
                 <div className="job-info">
                   <h2>{job["company"]}</h2>
@@ -72,27 +133,37 @@ function App() {
                     <h1>{job["title"]}</h1>
                   </a>
                   <div>
-                  <span>{job["type"]}</span>  
-                  <span className="divisor">-</span>
-                  <span>{job["location"]}</span>
+                    <span>{job["type"]}</span>
+                    <span className="divisor">-</span>
+                    <span>{job["location"]}</span>
                   </div>
                 </div>
               </div>
+              <hr />
               <div className="job-tags">
-                {defaultTags.map(tag => {
-                  if(job["description"].toUpperCase().indexOf(tag.toUpperCase()) !== -1){
-                    return(
-                    <button onClick={()=> {
-                      if(tags.indexOf(tag) === -1){
-                        setTags([...tags, tag])
-                      }
-                    }}>{tag}</button>
-                    )
+                {defaultTags.map((tag) => {
+                  if (
+                    job["description"]
+                      .toUpperCase()
+                      .indexOf(tag.toUpperCase()) !== -1
+                  ) {
+                    return (
+                      <button
+                        onClick={() => {
+                          if (tags.indexOf(tag) === -1) {
+                            setTags([...tags, tag]);
+                            filterJobs(tag);
+                          }
+                        }}
+                      >
+                        {tag}
+                      </button>
+                    );
                   }
                 })}
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
