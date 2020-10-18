59998e04d02c882c957c5cdd21b64a30699dd339
pedro-isacss <ss.pedroisac@gmail.com>
2020-10-18 09:35:51 -0300

    Add static-job-listings-master build

---
+++ portfolio/static-job-listings-master/build/precache-manifest.7946c10131e2c37192f1aff56a9df4ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f229764790bf5a115ff742c261bf79d1",
+    "url": "/portfolio/static-job-listings-master/build/index.html"
+  },
+  {
+    "revision": "e444d4619fec921ae094",
+    "url": "/portfolio/static-job-listings-master/build/static/css/main.dac112f3.chunk.css"
+  },
+  {
+    "revision": "f902f69e133178e6a666",
+    "url": "/portfolio/static-job-listings-master/build/static/js/2.3b6beb7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static-job-listings-master/build/static/js/2.3b6beb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e444d4619fec921ae094",
+    "url": "/portfolio/static-job-listings-master/build/static/js/main.ea048d8c.chunk.js"
+  },
+  {
+    "revision": "81177938f88810ba05f2",
+    "url": "/portfolio/static-job-listings-master/build/static/js/runtime-main.9fb8bc96.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/portfolio/static-job-listings-master/build/static/media/bg-header-desktop.6e2f7196.svg"
+  }
+]);
