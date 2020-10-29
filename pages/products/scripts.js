fc000f019bf1f018f4bbd89e6ef7a62940f5b457
Pedro Isac <ss.pedroisac@gmail.com>
2020-10-28 22:03:49 -0300

    Bug fixes

--- pages/products/scripts.js
+++ pages/products/scripts.js
@@ -1,6 +1,4 @@
 let dataProducts = [];
-let idsProductsElements = [];
-let countProductsElements = 0;
 
 /* RESPONSIVE MENU */
 let open = false;
@@ -17,78 +15,21 @@ $(".nav-button").click(function () {
 });
 
 /* CREATE ELEMENT PRODUCT */
-function createProductElement(title, link, img, value) {
-  countProductsElements++;
-  let productContainer = document.querySelector("div.products-container");
-
-  let elementContainer = document.createElement("a");
-  let imgElement = document.createElement("img");
-  let infoContainer = document.createElement("div");
-  let titleElement = document.createElement("h2");
-  let valueElement = document.createElement("span");
-
-  elementContainer.setAttribute("href", link);
-  elementContainer.setAttribute("class", "productsElement");
-  elementContainer.setAttribute(
-    "id",
-    "productsElement" + countProductsElements
+function createProductElement(title, link, img, value, type) {
+  let elementContainer = $(`<a href="${link}" class="productsElement"></a>`);
+  let imgElement = $(`<img src="${img}" alt="${title}" />`);
+  let infoContainer = $(
+    `<div class="info-productsElement-container flex-row-column-between-center"></div>`
   );
-  infoContainer.setAttribute(
-    "class",
-    "info-productsElement-container flex-row-column-between-center"
-  );
-  imgElement.setAttribute("src", img);
-  imgElement.setAttribute("alt", title);
-  titleElement.innerHTML = title;
-  valueElement.innerHTML = value;
+  let titleElement = $(`<h2>${title}</h2>`);
+  let valueElement = $(`<span>${value}</span>`);
+  let typeElement = $(`<span>${type}</span>`);
 
-  infoContainer.appendChild(titleElement);
-  infoContainer.appendChild(valueElement);
-  elementContainer.appendChild(imgElement);
-  elementContainer.appendChild(infoContainer);
+  infoContainer.append(typeElement, valueElement);
+  elementContainer.append(imgElement, infoContainer, titleElement);
 
-  productContainer.appendChild(elementContainer);
-  idsProductsElements = [
-    ...idsProductsElements,
-    "productsElement" + countProductsElements,
-  ];
+  $(".products-container").append(elementContainer);
 }
-
-/* ANIMATION TYPING */
-let indexName = 1;
-setInterval(function () {
-  if ($("#text").text() !== name && indexName < name.length) {
-    nameAtual += name[indexName];
-    $("#text").text(nameAtual);
-    indexName++;
-  } else {
-    nameAtual = "H";
-    $("#text").text(nameAtual);
-    indexName = 1;
-  }
-}, 400);
-
-/* ANIMATION PRODUCTS ELEMENT */
-$(window).scroll(function () {
-  for (let item of idsProductsElements) {
-    $("#" + item).css({ opacity: 0, transition: "2s" });
-    if (
-      $(window).scrollTop() >
-      $("#" + item).offset().top - $(window).height() / 1.2
-    ) {
-      $("#" + item).css({
-        "animation-name": "productsElementAnimationScroll",
-        "animation-duration": "2s",
-        "animation-iteration-count": 1,
-        opacity: 1,
-      });
-    } else {
-      $("#" + item).css({
-        animation: "none",
-      });
-    }
-  }
-});
 
 /* GET PRODUCTS ELEMENTS */
 fetch("../../data/products.json")
@@ -100,7 +41,8 @@ fetch("../../data/products.json")
         element["title"],
         element["link"],
         element["img"],
-        element["value"]
+        element["value"],
+        element["type"]
       );
     });
   });
