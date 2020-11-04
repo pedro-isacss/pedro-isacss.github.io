be4cf2b21f3af69a18e81b57f9f3ffb686102680
Pedro Isac <ss.pedroisac@gmail.com>
2020-11-04 18:49:48 -0300

    Add menu to  posts scripts

--- posts/scripts.js
+++ posts/scripts.js
@@ -10,3 +10,15 @@ $(".nav-button").click(function () {
     $(".nav-button-img").attr("src", "../images/icon-menu.png");
   }
 });
+
+//CREATE MENU
+let menu1 = $(`<a href="../index.html" class="nav-link">Home</a>`);
+let menu2 = $(
+  `<a href="https://app.jobzz.com.br/service/preview/efb2f900" class="nav-link">Services</a>`
+);
+let menu3 = $(
+  `<a href="https://front-ui.alumy.com/" class="nav-link">FrontUI</a>`
+);
+let menu4 = $(`<a href="../pages/blog/index.html" class="nav-link">Blog</a>`);
+
+$(".link-container").append(menu1, menu2, menu3, menu4);
