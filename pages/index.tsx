2f2b000e8f367f4873dfb03a2faa1e98db9e9dac
Pedro Isac <ss.pedroisac@gmail.com>
2021-01-25 13:46:39 -0300

    New design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import styles from "../styles/index.module.css";
 import Header from "../components/Header";
-import Links from "../components/Links";
 import Portfolio from "../components/Portfolio";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className={styles.index}>
-      <Header />
-      <Links />
-      <hr className="divisor" />
-      <Portfolio />
-      <Footer />
+    <div>
+      <div className={styles.section}>
+        <div className={styles.subsection}>
+          <Header />
+        </div>
+        <div className={`${styles.subsection} ${styles.bggradentblue}`}>
+          <Portfolio />
+        </div>
+      </div>
     </div>
   );
 }
