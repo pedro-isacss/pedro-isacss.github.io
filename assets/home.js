ba644e650f7d8adfaf72f0875817aacefdd73761
Pedro Isac <ss.pedroisac@gmail.com>
2021-06-10 09:07:29 -0300

    Slides to show websites

--- assets/home.js
+++ assets/home.js
@@ -1,30 +1,25 @@
 const divSites = document.getElementById("sites");
 
-function getSites(until) {
-  fetch("../data/sites.json")
-    .then((response) => response.json())
-    .then((res) => {
-      res.slice(0, until).map((site) => {
-        const link = document.createElement("a");
-        const img = document.createElement("img");
-        link.setAttribute("href", site.link);
-        link.setAttribute("class", "col m-3");
-        link.setAttribute("target", "_blank");
-        img.setAttribute("src", site.img);
-        img.setAttribute("class", "rounded shadow-lg");
-        img.style.maxWidth = "250px";
-        img.style.width = "100%";
-        link.style.maxWidth = "250px";
-        link.style.width = "100%";
-        link.style.padding = 0;
-        link.appendChild(img);
-        divSites.appendChild(link);
-      });
+fetch("../data/sites.json")
+  .then((response) => response.json())
+  .then((res) => {
+    res.slice(0, 10).map((site) => {
+      const link = document.createElement("a");
+      const img = document.createElement("img");
+      link.setAttribute("href", site.link);
+      link.setAttribute("class", "col m-3");
+      link.setAttribute("target", "_blank");
+      img.setAttribute("src", site.img);
+      img.setAttribute("class", "rounded");
+      img.style.maxWidth = "250px";
+      img.style.width = "100%";
+      link.style.maxWidth = "250px";
+      link.style.width = "100%";
+      link.style.display = "flex";
+      link.style.justifyContent = "center";
+      link.style.alignItems = "center";
+      link.style.padding = 0;
+      link.appendChild(img);
+      divSites.appendChild(link);
     });
-}
-
-if (window.innerWidth > 530) {
-  getSites(3);
-} else {
-  getSites(1);
-}
+  });
