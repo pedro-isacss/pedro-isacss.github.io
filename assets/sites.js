eb274a60235ff39cd39c5bd84f33395ad39fc394
Pedro Isac <ss.pedroisac@gmail.com>
2021-06-16 16:48:51 -0300

    Sites page

---
+++ assets/sites.js
@@ -0,0 +1,24 @@
+const divSites = document.getElementById("sites");
+
+fetch("../data/sites.json")
+  .then((response) => response.json())
+  .then((res) => {
+    res.map((site) => {
+      const link = document.createElement("a");
+      const img = document.createElement("img");
+      link.setAttribute("href", site.link);
+      link.setAttribute("target", "_blank");
+      img.setAttribute("src", site.img);
+      img.setAttribute("class", "rounded");
+      img.style.maxWidth = "250px";
+      img.style.width = "100%";
+      link.style.maxWidth = "250px";
+      link.style.width = "100%";
+      link.style.display = "flex";
+      link.style.justifyContent = "center";
+      link.style.alignItems = "center";
+      link.style.padding = 0;
+      link.appendChild(img);
+      divSites.appendChild(link);
+    });
+  });
